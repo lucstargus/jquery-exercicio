2a2a07860cd5ec589df877739a73d7688d925005
Lucas <158581359+lucstargus@users.noreply.github.com>
2025-12-22 19:50:16 +0000

    completando

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cep').mask('00000-000')
+    $('#cpf').mask('000.000.000-00')
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required: true
+            },
+            email:{
+                required: true
+            },
+            telefone:{
+                required: true
+            },
+            cpf:{
+                required: true
+            },
+            endereco:{
+                required: true
+            },
+            cep:{
+                required: true
+            }
+        }
+    })
+})
